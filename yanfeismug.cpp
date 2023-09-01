b9f42d62e769ceee4dadaa3cd85b077a426d5be1
Ei <59531041+Mewtwo2387@users.noreply.github.com>
2023-09-01 19:25:21 +0800

    yanfeismug

---
+++ yanfeismug.cpp
@@ -0,0 +1,17 @@
+#define sexually_transmitted_disease std
+#define implicit int
+#define none main
+#define transmit cout
+#define arrow_left <<
+#define fuck return
+#define you 0
+#define yanfeismug ;
+
+#include <iostream>
+
+
+implicit none()
+{
+    sexually_transmitted_disease::transmit arrow_left "goodbye world" yanfeismug
+    fuck you yanfeismug
+}
