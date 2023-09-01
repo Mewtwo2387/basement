2c6ef843969cc27d35c9e16c03f84f87d7682ed4
implicit none <null000000008@gmail.com>
2023-09-01 19:05:46 +0800

    Change sample code dir name

--- src/hello-world.f90
+++
@@ -1,8 +0,0 @@
-program Hello
-    implicit none
-    character(len=32) :: name
-
-    write(*, "(a)", advance="no") "What's your name? 
-    read *, name
-    print "(3a)", "Hello, ", trim(name), "!"
-end program Hello
