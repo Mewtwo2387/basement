6e18474fa3a3411f757ded5cd9bbe2b9690e9026
implicit none <null000000008@gmail.com>
2023-09-01 19:10:15 +0800

    Fix done goofed rebase

---
+++ implicit-lang-sample/hello-world.f90
@@ -0,0 +1,8 @@
+program Hello
+    implicit none
+    character(len=32) :: name
+
+    write(*, "(a)", advance="no") "What's your name? 
+    read *, name
+    print "(3a)", "Hello, ", trim(name), "!"
+end program Hello
