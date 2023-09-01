cc7e77f1a5541aff53664796013ec517e4f27745
ei <mewtwo2387@gmail.com>
2023-09-01 19:46:29 +0800

    very good code

--- yanfeismug.cpp
+++ yanfeismug.cpp
@@ -6,12 +6,27 @@
 #define fuck return
 #define you 0
 #define yanfeismug ;
-
+#define weis_excalibur long
+#define wool string 
+#define maybe if
+#define otherwise else
+#define ： {
+#define oh }
 #include <iostream>
 
 
+sexually_transmitted_disease::wool aaaaaaa(weis_excalibur weis_excalibur implicit a){
+    maybe(a==1) ：
+        fuck "goodbye world" yanfeismug
+    oh otherwise ：
+        fuck "hello world" yanfeismug
+    oh
+}
+
 implicit none()
 {
-    sexually_transmitted_disease::transmit arrow_left "goodbye world" yanfeismug
+    weis_excalibur weis_excalibur implicit suPer_lOng___vAriable_name = 1 yanfeismug
+    
+    sexually_transmitted_disease::transmit arrow_left aaaaaaa(suPer_lOng___vAriable_name) yanfeismug
     fuck you yanfeismug
 }
