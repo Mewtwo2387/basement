a5eee7b265821937abd48d37f182680c7a3f7b30
implicit none <null000000008@gmail.com>
2023-09-06 20:17:46 +0800

    Fix loop skipping logic

--- implicit-brainfuck/main.c
+++ implicit-brainfuck/main.c
@@ -17,12 +17,13 @@
 #include "string_type.h"
 
 #define MAX_MEMORY_SIZE 1024
+#define MAX_STACK_SIZE 1024
 
 const char *valid_char = "+-<>.,[]";
 
 char data[MAX_MEMORY_SIZE] = { '\0' };
 string_t instruction;
-size_t stack[MAX_MEMORY_SIZE] = { (size_t)0 };
+size_t stack[MAX_STACK_SIZE] = { (size_t)0 };
 
 /* Pointers */
 size_t data_ptr  = 0;
@@ -59,53 +60,70 @@ int main(size_t argc, const char* argv[]) {
     fclose(input_file_ptr);
 
     /* Evaluate the program */
-    while ( (c = instruction.data[instr_ptr]) != '\0' ) {
+    while ( instr_ptr < instruction.len ) {
+        c = instruction.data[instr_ptr];
+
         /* Stack manipulating instructions */
         switch (c) {
-            case '[':
-                if (data[data_ptr] == 0) {
-                    // Skip the entire loop if 0
-                    while ( instruction.data[instr_ptr++] != ']' ) {}
-                } else {
-                    stack[stack_ptr++] = ++instr_ptr;
+        case '[':
+            if (data[data_ptr] == 0) {
+                // Skip the entire loop if 0
+                int bracket_lvl = 1;
+                while (instr_ptr < instruction.len && bracket_lvl > 0) {
+                    c = instruction.data[++instr_ptr];
+                    if (c == '[')
+                        ++bracket_lvl;
+                    if (c == ']')
+                        --bracket_lvl;
                 }
-                continue;
-            case ']':
-                if (data[data_ptr] == 0) {
-                    ++instr_ptr;
-                } else {
-                    if (stack_ptr == 0) {
-                        fprintf(stderr, "stack underflow!\n");
-                        graceful_exit(1);
-                    }
-                    instr_ptr = stack[stack_ptr - 1];
+                if (instr_ptr == instruction.len && bracket_lvl > 0) {
+                    fprintf(stderr, "mismatched bracket pairs!\n");
+                    graceful_exit(1);
                 }
-                continue;
+            } else {
+                if (stack_ptr == MAX_STACK_SIZE) {
+                    fprintf(stderr, "stack overflow!\n");
+                    graceful_exit(1);
+                }
+                stack[stack_ptr++] = ++instr_ptr;
+            }
+            continue;
+        case ']':
+            if (data[data_ptr] == 0) {
+                ++instr_ptr;
+            } else {
+                if (stack_ptr == 0) {
+                    fprintf(stderr, "stack underflow!\n");
+                    graceful_exit(1);
+                }
+                instr_ptr = stack[stack_ptr - 1];
+            }
+            continue;
         }
 
         /* Data manipulating instructions */
         switch (c) {
-            case '+':
-                ++data[data_ptr];
-                break;
-            case '-':
-                --data[data_ptr];
-                break;
-            case '>':
-                ++data_ptr;
-                break;
-            case '<':
-                --data_ptr;
-                break;
-            case '.':
-                putc(data[data_ptr], stdout);
-                break;
-            case ',':
-                data[data_ptr] = getc(stdin);
-                break;
-            default:
-                fprintf(stderr, "you fucking broke it, didnt ya?\n");
-                graceful_exit(1);
+        case '+':
+            ++data[data_ptr];
+            break;
+        case '-':
+            --data[data_ptr];
+            break;
+        case '>':
+            ++data_ptr;
+            break;
+        case '<':
+            --data_ptr;
+            break;
+        case '.':
+            putc(data[data_ptr], stdout);
+            break;
+        case ',':
+            data[data_ptr] = getc(stdin);
+            break;
+        default:
+            fprintf(stderr, "you fucking broke it, didnt ya?\n");
+            graceful_exit(1);
         }
         ++instr_ptr;
     }
