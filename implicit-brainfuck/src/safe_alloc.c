1093a2d0ddefcec2043d5aad5a939e7f1dd361c7
implicit none <null000000008@gmail.com>
2023-09-10 14:29:10 +0800

    Change alloc failure error msg

--- implicit-brainfuck/src/safe_alloc.c
+++ implicit-brainfuck/src/safe_alloc.c
@@ -1,27 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+#include "error.h"
 #include "safe_alloc.h"
 
-#define ALLOC_GUARD(ptr, name)                                  \
-    if (ptr == NULL) {                                          \
-        fprintf(stderr, "<Internal error: %s failed>\n", name); \
-        exit(EXIT_FAILURE);                                     \
-    }
+#define ALLOC_GUARD(ptr, name)                                    \
+    if (ptr == NULL)                                              \
+        throw_error(CUSTOM, (struct memory){ 0 }, name " failed", \
+                    false, false);                                \
 
 void *safe_malloc(size_t size) {
     void *ptr = malloc(size);
-    ALLOC_GUARD(ptr, "malloc")
+    ALLOC_GUARD(ptr, "`malloc`")
     return ptr;
 }
 
 void *safe_calloc(size_t num, size_t size) {
     void *ptr = calloc(num, size);
-    ALLOC_GUARD(ptr, "calloc")
+    ALLOC_GUARD(ptr, "`calloc`")
     return ptr;
 }
 
 void *safe_realloc(void *ptr, size_t new_size) {
     void *new_ptr = realloc(ptr, new_size);
-    ALLOC_GUARD(new_ptr, "realloc")
+    ALLOC_GUARD(new_ptr, "`realloc`")
     return new_ptr;
 }
