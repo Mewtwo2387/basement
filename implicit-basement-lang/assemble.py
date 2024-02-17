52d6f4febcaa66e8709fb6a6f5644e43d2a6876f
implicit none <null000000008@gmail.com>
2024-02-17 19:39:29 +0800

    BS lang assembler: Improve the assembler test script

--- implicit-basement-lang/assemble.py
+++ implicit-basement-lang/assemble.py
@@ -1,8 +1,9 @@
-import os
-
 from assembler.parser import parse
-from assembler.token.scope_elem import ScopeStart, ScopeEnd
 from assembler.token.delim import EndOfLine
+from assembler.token.scope_elem import ScopeStart, ScopeEnd
+import assembler.semantics as semantics
+
+import os
 
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -16,35 +17,44 @@ def main():
     parse_result = parse(code)
 
     if isinstance(parse_result, tuple):
-        output, _ = parse_result
+        output, struct_dict = parse_result
 
-        indent = "    "
-        print("Parsed code:", "=" * 80, sep="\n")
+        divider = "=" * 80
+        print("Parsed code:", divider, sep="\n")
+        print_tokens(output)
+        print("\n" + divider)
 
-        indent_lvl = 1
-        to_indent = True
-        for token in output:
-            if to_indent:
-                print(indent * indent_lvl, sep="", end="")
+        rpn_tokens = semantics.analyze_semantics(output, struct_dict)
+        print_tokens(rpn_tokens)
+        print("\n" + divider)
 
-            to_indent = False
-            match token:
-                case ScopeStart():
-                    print("\n", indent * indent_lvl, token, sep="", end="")
-                    indent_lvl += 1
-                    print("\n", indent * indent_lvl, sep="", end="")
-                case ScopeEnd():
-                    indent_lvl -= 1
-                    print(f"{chr(27)}[100D", indent * indent_lvl, token, sep="")
-                    to_indent = True
-                case EndOfLine():
-                    print(token)
-                    to_indent = True
-                case _:
-                    print(token, end=", ")
-        print("\n" + "=" * 80)
     else:
         print("Error parsing")
+
+
+def print_tokens(arr):
+    indent = "    "
+    indent_lvl = 1
+    to_indent = True
+    for token in arr:
+        if to_indent:
+            print(indent * indent_lvl, sep="", end="")
+
+        to_indent = False
+        match token:
+            case ScopeStart():
+                print("\n", indent * indent_lvl, token, sep="", end="")
+                indent_lvl += 1
+                print("\n", indent * indent_lvl, sep="", end="")
+            case ScopeEnd():
+                indent_lvl -= 1
+                print(f"{chr(27)}[100D", indent * indent_lvl, token, sep="")
+                to_indent = True
+            case EndOfLine():
+                print(token)
+                to_indent = True
+            case _:
+                print(token, end=", ")
 
 
 def print_bytes(arr):
