1010aebd60287dcdc59ee997a35f5486a7847e41
implicit none <null000000008@gmail.com>
2024-02-25 19:08:00 +0800

    BS lang assembler: Simplify token printing for debugging

--- implicit-basement-lang/assemble.py
+++ implicit-basement-lang/assemble.py
@@ -4,6 +4,7 @@ from assembler.token.scope_elem import ScopeStart, ScopeEnd
 import assembler.semantics as semantics
 
 import os
+from math import log10
 
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -21,40 +22,30 @@ def main():
 
         divider = "=" * 80
         print("Parsed code:", divider, sep="\n")
-        print_tokens(output)
+        list_tokens(output)
         print("\n" + divider)
 
         rpn_tokens = semantics.analyze_semantics(output, struct_dict)
-        print_tokens(rpn_tokens)
+        list_tokens(rpn_tokens)
         print("\n" + divider)
 
     else:
         print("Error parsing")
 
 
-def print_tokens(arr):
+def list_tokens(arr):
     indent = "    "
     indent_lvl = 1
-    to_indent = True
-    for token in arr:
-        if to_indent:
-            print(indent * indent_lvl, sep="", end="")
+    index_padding_len = (int(log10(len(arr))) + 1) if arr else 1
+    for i, token in enumerate(arr):
+        if isinstance(token, ScopeEnd):
+            indent_lvl -= 1
+        print(f"[{str(i).zfill(index_padding_len)}]{indent * indent_lvl}",
+              end="")
+        if isinstance(token, ScopeStart):
+            indent_lvl += 1
 
-        to_indent = False
-        match token:
-            case ScopeStart():
-                print("\n", indent * indent_lvl, token, sep="", end="")
-                indent_lvl += 1
-                print("\n", indent * indent_lvl, sep="", end="")
-            case ScopeEnd():
-                indent_lvl -= 1
-                print(f"{chr(27)}[100D", indent * indent_lvl, token, sep="")
-                to_indent = True
-            case EndOfLine():
-                print(token)
-                to_indent = True
-            case _:
-                print(token, end=", ")
+        print(token)
 
 
 def print_bytes(arr):
